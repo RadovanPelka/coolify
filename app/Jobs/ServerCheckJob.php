4e09d0253ad01b44e900feeb2e4acfcbe32eca50
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-07-14 13:26:54 +0200

    Update ServerCheckJob.php

--- app/Jobs/ServerCheckJob.php
+++ app/Jobs/ServerCheckJob.php
@@ -9,13 +9,14 @@ use App\Actions\Server\StartLogDrain;
 use App\Models\Server;
 use App\Notifications\Container\ContainerRestarted;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeEncrypted;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\Middleware\WithoutOverlapping;
 use Illuminate\Queue\SerializesModels;
 
-class ServerCheckJob implements ShouldQueue
+class ServerCheckJob implements ShouldBeEncrypted, ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
@@ -27,77 +28,76 @@ class ServerCheckJob implements ShouldQueue
 
     public function middleware(): array
     {
-        return [(new WithoutOverlapping('server-check-'.$this->server->uuid))->dontRelease()];
+        return [(new WithoutOverlapping('server-check-'.$this->server->uuid))->expireAfter(60)->dontRelease()];
     }
 
     public function __construct(public Server $server) {}
 
     public function handle()
     {
-        // try {
-        // if ($this->server->serverStatus() === false) {
-        //     return 'Server is not reachable or not ready.';
-        // }
-
-        if (! $this->server->isSwarmWorker() && ! $this->server->isBuildServer()) {
-            ['containers' => $this->containers, 'containerReplicates' => $containerReplicates] = $this->server->getContainers();
-            if (is_null($this->containers)) {
-                return 'No containers found.';
+        try {
+            if ($this->server->serverStatus() === false) {
+                return 'Server is not reachable or not ready.';
             }
-        }
 
-        //     GetContainersStatus::run($this->server, $this->containers, $containerReplicates); --> THIS CUASES 2 SSH sessions to be opened??
-
-        // if ($this->server->isSentinelEnabled()) {
-        //     CheckAndStartSentinelJob::dispatch($this->server);
-        // }Yes, but they work the same regardless of the balancing strategy. So, if you set `balance=false`, you can still set `minProcesses` and `maxProcesses`. If you set `minProcesses=1 `and `maxProcesses=4`, 1 process will run in total, even if you have two queues because of balance being set to false. If there is load, Horizon will add up to 3 more workers (total of 4), as it would with any other strategy. This should be reflected somehow, as the current one is confusing to me.
-
-        // if ($this->server->isLogDrainEnabled()) {
-        //     $this->checkLogDrainContainer();
-        // }
-
-        if ($this->server->proxySet() && ! $this->server->proxy->force_stop) {
-            $this->server->proxyType();
-            $foundProxyContainer = $this->containers->filter(function ($value, $key) {
-                if ($this->server->isSwarm()) {
-                    return data_get($value, 'Spec.Name') === 'coolify-proxy_traefik';
-                } else {
-                    return data_get($value, 'Name') === '/coolify-proxy';
+            if (! $this->server->isSwarmWorker() && ! $this->server->isBuildServer()) {
+                ['containers' => $this->containers, 'containerReplicates' => $containerReplicates] = $this->server->getContainers();
+                if (is_null($this->containers)) {
+                    return 'No containers found.';
                 }
-            })->first();
-            if (! $foundProxyContainer) {
-                try {
-                    $shouldStart = CheckProxy::run($this->server);
-                    if ($shouldStart) {
-                        StartProxy::run($this->server, async: false);
-                        $this->server->team?->notify(new ContainerRestarted('coolify-proxy', $this->server));
+                GetContainersStatus::run($this->server, $this->containers, $containerReplicates); // --> THIS CAUSES 2 SSH sessions to be opened??
+
+                if ($this->server->isSentinelEnabled()) {
+                    CheckAndStartSentinelJob::dispatch($this->server);
+                }
+
+                if ($this->server->isLogDrainEnabled()) {
+                    $this->checkLogDrainContainer();
+                }
+
+                if ($this->server->proxySet() && ! $this->server->proxy->force_stop) {
+                    $this->server->proxyType();
+                    $foundProxyContainer = $this->containers->filter(function ($value, $key) {
+                        if ($this->server->isSwarm()) {
+                            return data_get($value, 'Spec.Name') === 'coolify-proxy_traefik';
+                        } else {
+                            return data_get($value, 'Name') === '/coolify-proxy';
+                        }
+                    })->first();
+                    if (! $foundProxyContainer) {
+                        try {
+                            $shouldStart = CheckProxy::run($this->server);
+                            if ($shouldStart) {
+                                StartProxy::run($this->server, async: false);
+                                $this->server->team?->notify(new ContainerRestarted('coolify-proxy', $this->server));
+                            }
+                        } catch (\Throwable $e) {
+                        }
+                    } else {
+                        $this->server->proxy->status = data_get($foundProxyContainer, 'State.Status');
+                        $this->server->save();
+                        $connectProxyToDockerNetworks = connectProxyToNetworks($this->server);
+                        instant_remote_process($connectProxyToDockerNetworks, $this->server, false);
                     }
-                } catch (\Throwable $e) {
                 }
-            } else {
-                $this->server->proxy->status = data_get($foundProxyContainer, 'State.Status');
-                $this->server->save();
-                $connectProxyToDockerNetworks = connectProxyToNetworks($this->server);
-                instant_remote_process($connectProxyToDockerNetworks, $this->server, false);
             }
+        } catch (\Throwable $e) {
+            return handleError($e);
         }
-        // }
-        // } catch (\Throwable $e) {
-        //     return handleError($e);
     }
 
-    // private function checkLogDrainContainer()
-    // {
-    //     $foundLogDrainContainer = $this->containers->filter(function ($value, $key) {
-    //         return data_get($value, 'Name') === '/coolify-log-drain';
-    //     })->first();
-    //     if ($foundLogDrainContainer) {
-    //         $status = data_get($foundLogDrainContainer, 'State.Status');
-    //         if ($status !== 'running') {
-    //             StartLogDrain::dispatch($this->server);
-    //         }
-    //     } else {
-    //         StartLogDrain::dispatch($this->server);
-    //     }
-    // }
+    private function checkLogDrainContainer()
+    {
+        $foundLogDrainContainer = $this->containers->filter(function ($value, $key) {
+            return data_get($value, 'Name') === '/coolify-log-drain';
+        })->first();
+        if ($foundLogDrainContainer) {
+            $status = data_get($foundLogDrainContainer, 'State.Status');
+            if ($status !== 'running') {
+                StartLogDrain::dispatch($this->server);
+            }
+        } else {
+            StartLogDrain::dispatch($this->server);
+        }
+    }
 }
